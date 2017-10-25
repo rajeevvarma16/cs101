f496deacd1beaac7857d418a7310ea743a589cf3
Rajeev <varma410@gmail.com>
2017-10-25 08:17:13 +0530

    modified age_in_days trying with a different method

--- age_in_days.py
+++ age_in_days.py
@@ -1,51 +1,55 @@
-def is_leap_year(n):
-    if (n%4 != 0):
+def is_leap_year(year):
+    if (year%4 != 0):
         return False
-    elif (n%100 != 0):
+    elif (year%100 != 0):
         return True
-    elif (n%400 != 0):
+    elif (year%400 != 0):
         return False
     else:
         return True
-def days_in_years(year):
+def days_of_months(year):
     if is_leap_year(year):
         daysOfMonths = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        days_in_year = sum(daysOfMonths)
-        return daysOfMonths,days_in_year
+        return daysOfMonths
     else:
         daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        days_in_year = sum(daysOfMonths)
-        return daysOfMonths,days_in_year
-        
-def required_days_in_year(year, month, day):
-    months,no_of_days = days_in_years(year)
-    if month-1 !=0:
-        total_no_days_passed = sum(months[:month-1]) + day
+        return daysOfMonths
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    daysOfMonths = days_of_months(year)
+    if day < daysOfMonths[month-1]:
+        return year, month, day + 1
     else:
-        total_no_days_passed = day
-    remaining = no_of_days - total_no_days_passed
-    return remaining,total_no_days_passed
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
-    years = []
-    days = []
-    for i in range(year2 - year1):
-        years.append(year1+i)
-    years = years[1:]
-    print years
-    if len(years) != 0:
-        for year in years:
-            days.append(days_in_years(year)[1])   
-    if (year2 - year1) == 0:
-        remaining_days_in_year1,days_passed_in_year1 = required_days_in_year(year1, month1, day1)
-        remaining_days_in_year2,days_passed_in_year2 = required_days_in_year(year2, month2, day2)
-        return days_passed_in_year2-days_passed_in_year1
-    if (year2 - year1) != 0:
-        remaining_days_in_year1,days_passed_in_year1 = required_days_in_year(year1, month1, day1)
-        remaining_days_in_year2,days_passed_in_year2 = required_days_in_year(year2, month2, day2)
-        return remaining_days_in_year1+days_passed_in_year2+sum(days)
-# Test routine
-print daysBetweenDates(1900,1,1,1999,12,31)
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+print daysBetweenDates(2012,1,1,2012,2,28)
+assert daysBetweenDates(2012,1,1,2012,2,28)== 57
+print "Test passed"
 '''
 def test():
     test_cases = [((2012,1,1,2012,2,28), 58), 
@@ -53,6 +57,7 @@ def test():
                   ((2011,6,30,2012,6,30), 366),
                   ((2011,1,1,2012,8,8), 585 ),
                   ((1900,1,1,1999,12,31), 36523)]
+    
     for (args, answer) in test_cases:
         result = daysBetweenDates(*args)
         if result != answer:
@@ -61,4 +66,6 @@ def test():
             print "Test case passed!"
 
 test()
+
+
 '''
